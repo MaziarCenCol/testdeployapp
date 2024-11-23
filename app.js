0f5b6ce3a94f5afba97bff0707ddb78d071e423d
Maziar Hassanzadeh <maziar.hassanzadeh@gmail.com>
2024-11-23 09:56:04 -0500

    update 1

---
+++ app.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+app.get("/", (req, res)=>{
+    res.send("Hello world")
+})
+app.get("/test", (req, res)=>{
+    res.send("Hello world test")
+})
+const PORT = 8000;
+app.listen(PORT, ()=>{
+    console.log("Server is running on port ${PORT}")
+})
